59fd82f117e393ee0b4fb4fe10128438061c217c
Kyriakos Noves <kn_macpro-13zoll@MacBook-Air-von-Kyriakos.local>
2025-07-16 19:03:30 +0200

    Initial commit

---
+++ web_dashboard.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    images = [img for img in os.listdir('.') if img.endswith('_forecast.png')]
+    return render_template('index.html', images=images)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
