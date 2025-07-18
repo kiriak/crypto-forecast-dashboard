a5f804998ecc9961e5890b34bb0c64621ef820c5
Kyriakos Noves <kn_macpro-13zoll@MacBook-Air-von-Kyriakos.local>
2025-07-18 23:33:47 +0200

    final working version

--- web_dashboard.py
+++ web_dashboard.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "✅ Crypto Dashboard is running!"
+def index():
+    images = [
+        "bitcoin_forecast.png",
+        "ethereum_forecast.png",
+        "solana_forecast.png"
+    ]
+    return render_template('index.html', images=images)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8501)
